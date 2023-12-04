3837eee797c9f3346c589d994158639de2de35a2
Gyula Matics <mgyula@gmail.com>
2023-12-04 18:10:04 +0100

    found out how to do power:)

--- 041.py
+++ 041.py
@@ -9,20 +9,16 @@ for line in lines:
     winning_numbers_string = split_numbers[0]
     my_numbers_strnig = split_numbers[1]
 
-    winning_numbers = {winning_numbers_string[start:start+3] for start in range(0, len(winning_numbers_string), 3)}
-    my_numbers = {my_numbers_strnig[start:start+3] for start in range(0, len(my_numbers_strnig), 3)}
+    winning_numbers = {winning_numbers_string[start:start + 3] for start in range(0, len(winning_numbers_string), 3)}
+    my_numbers = {my_numbers_strnig[start:start + 3] for start in range(0, len(my_numbers_strnig), 3)}
     wins = winning_numbers & my_numbers
 
-    if len(wins) == 0:
-        points = 0
-    else:
-        points = 1
-        for p in wins:
-            points *= 2
+    if len(wins) > 0:
+        points = 2 ** (len(wins) - 1)
+        all_points.append(points)
+
     print(wins, points)
 
-    all_points.append(points)
-
-print(sum(all_points)/2)
+print(sum(all_points))
 
 # 26426
