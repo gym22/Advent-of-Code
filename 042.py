d7aa6a867ff37cb8bbad0226ca68b4ef54901bb4
Gyula Matics <mgyula@gmail.com>
2023-12-04 18:11:38 +0100

    learned about unpacking and list comprehension

--- 042.py
+++ 042.py
@@ -1,22 +1,24 @@
+import re
+
 with open("day4input1.txt") as file:
-    cards = list(map(lambda x: {"split_numbers": x.strip().split(":")[1].split(" |"), "copies": 1}, file))
+    cards = [{"numbers": x.strip().split(":")[1], "copies": 1} for x in file]
 
 all_points = []
 card_copies = {}
 
 for card_no, card in enumerate(cards):
     copies_of_this_card = card["copies"]
-    winning_numbers_string = card["split_numbers"][0]
-    my_numbers_strnig = card["split_numbers"][1]
+    winning_numbers_string, my_numbers_string = card["numbers"].split(" |")
 
-    winning_numbers = {winning_numbers_string[start:start+3] for start in range(0, len(winning_numbers_string), 3)}
-    my_numbers = {my_numbers_strnig[start:start+3] for start in range(0, len(my_numbers_strnig), 3)}
+    winning_numbers = set(re.findall(r"(\d+)", winning_numbers_string))
+    my_numbers = set(re.findall(r"(\d+)", my_numbers_string))
+
     wins = winning_numbers & my_numbers
 
     number_of_next_cards = len(wins)
 
     print(copies_of_this_card, number_of_next_cards, wins)
-    for next_card in cards[card_no+1:card_no+1+number_of_next_cards]:
+    for next_card in cards[card_no + 1:card_no + 1 + number_of_next_cards]:
         next_card["copies"] += copies_of_this_card
 
 print(sum(c["copies"] for c in cards))
