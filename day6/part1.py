d1547942bbb1b88e2f6afaad959e63b6d13f9d56
Gyula Matics <mgyula@gmail.com>
2023-12-06 09:25:03 +0100

    Day 6: actually, rounding works just fine

--- day6/part1.py
+++ day6/part1.py
@@ -3,12 +3,7 @@ import math
 inputs = [[60,475],[94,2138],[78,1015],[82,1650]]
 outputs = []
 for T,D in inputs:
-    halfT = T/2
-    sq = math.sqrt(T**2-4*D)/2
-
-    lower = math.floor(halfT - sq)
-    upper = math.floor(halfT + sq)
-    range = upper - lower
+    range = round(math.sqrt(T ** 2 - 4 * D))
     outputs.append(range)
     print(range)
 
--- day6/part2.py
+++ day6/part2.py
@@ -2,10 +2,5 @@ import math
 
 T = 60947882
 D = 475213810151650
-halfT = T/2
-sq = math.sqrt(T**2-4*D)/2
-
-lower = math.floor(halfT - sq)
-upper = math.floor(halfT + sq)
-range = upper - lower
+range = round(math.sqrt(T ** 2 - 4 * D))
 print(range)
