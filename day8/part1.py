18c8123471bde1d3388ece0c5259f1939d1e56ea
Gyula Matics <mgyula@gmail.com>
2023-12-08 15:25:15 +0100

    Day 8 Part 1

---
+++ day8/part1.py
@@ -0,0 +1,22 @@
+import re
+
+with open("input.txt") as file:
+    rawinstructions, rawmaps = file.read().split('\n\n')
+
+    instructions = list(rawinstructions)
+    maps = {x[0]: {"L": x[1], "R": x[2]} for x in re.findall('(\\w{3}) = \\((\\w{3}). (\\w{3})', rawmaps)}
+
+# print(maps)
+location = "AAA"
+finallocation = "ZZZ"
+steps = 0
+
+while location != finallocation:
+    instruction = instructions.pop(0)
+    instructions.append(instruction)
+
+    location = maps[location][instruction]
+    print(location, instruction)
+    steps += 1
+
+print(steps)
