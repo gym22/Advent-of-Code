99a9be1d47797be5bcb886c20ce1c4147cd195d5
Gyula Matics <mgyula@gmail.com>
2023-12-15 14:47:06 +0100

    Day 13 part 1 simplify with transpose

--- day13/part1.py
+++ day13/part1.py
@@ -1,11 +1,8 @@
 with open("input.txt") as file:
-    fields = [[y for y in x.split("\n")] for x in file.read().split("\n\n")]
-
-horizontals = []
-verticals = []
+    fields = [[list(y) for y in x.split("\n")] for x in file.read().split("\n\n")]
 
 
-def find_horizontals(field):
+def find_reflections(field):
     for i in range(0, len(field) - 1):
         if field[i] == field[i + 1]:
             s = True
@@ -13,29 +10,14 @@ def find_horizontals(field):
                 if i - r >= 0 and i + 1 + r < len(field) and field[i - r] != field[i + 1 + r]:
                     s = False
             if s:
-                horizontals.append(i + 1)
+                return i + 1
+    return 0
 
-
-def compare_vertical(field, c1, c2):
-    for line in field:
-        if line[c1] != line[c2]:
-            return False
-    return True
-
-
-def find_verticals(field):
-    for i in range(0, len(field[0]) - 1):
-        if compare_vertical(field, i, i + 1):
-            s = True
-            for r in range(1, min(i, len(field[0]) - i) + 1):
-                if i - r >= 0 and i + 1 + r < len(field[0]) and not compare_vertical(field, i - r, i + 1 + r):
-                    s = False
-            if s:
-                verticals.append(i + 1)
-
-
+sum = 0
 for field in fields:
-    find_horizontals(field)
-    find_verticals(field)
+    sum += 100 * find_reflections(field)
 
-print(sum(horizontals) * 100 + sum(verticals))
+    field = list(zip(*field))
+    sum += find_reflections(field)
+
+print(sum)
