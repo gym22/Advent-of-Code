169a7e25a02319b247faa386d7da9e6b7ba05ab5
Gyula Matics <mgyula@gmail.com>
2023-12-15 16:29:47 +0100

    Use transpose in part2 as well

--- day13/part2.py
+++ day13/part2.py
@@ -1,35 +1,13 @@
 with open("input.txt") as file:
     fields = [[list(y) for y in x.split("\n")] for x in file.read().split("\n\n")]
 
-horizontals = []
-verticals = []
 
-
-def find_horizontals(field, original_h):
+def find_reflections(field, original_h):
     for i in range(0, len(field) - 1):
         if i != original_h and field[i] == field[i + 1]:
             s = True
             for r in range(1, min(i, len(field) - i) + 1):
                 if i - r >= 0 and i + 1 + r < len(field) and field[i - r] != field[i + 1 + r]:
-                    s = False
-            if s:
-                return i + 1
-    return 0
-
-
-def compare_vertical(field, c1, c2):
-    for line in field:
-        if line[c1] != line[c2]:
-            return False
-    return True
-
-
-def find_verticals(field, original_v):
-    for i in range(0, len(field[0]) - 1):
-        if i != original_v and compare_vertical(field, i, i + 1):
-            s = True
-            for r in range(1, min(i, len(field[0]) - i) + 1):
-                if i - r >= 0 and i + 1 + r < len(field[0]) and not compare_vertical(field, i - r, i + 1 + r):
                     s = False
             if s:
                 return i + 1
@@ -43,27 +21,29 @@ def flip(f):
         f = "#"
     return f
 
+def smudge(field):
+        h = 0
+        original_h = find_reflections(field, -1) - 1
 
-for field in fields:
-    h = 0
-    v = 0
-    original_h = find_horizontals(field, -1) - 1
-    original_v = find_verticals(field, -1) - 1
-
-    for i in range(0, len(field)):
-        if h or v:
-            break
-        for r in range(0, len(field[0])):
-            if h or v:
-                break
-
-            field[i][r] = flip(field[i][r])
-            h = find_horizontals(field, original_h)
+        for i in range(0, len(field)):
             if h:
-                horizontals.append(h)
-            v = find_verticals(field, original_v)
-            if v:
-                verticals.append(v)
-            field[i][r] = flip(field[i][r])
+                break
+            for r in range(0, len(field[0])):
+                if h:
+                    break
 
-print(sum(horizontals) * 100 + sum(verticals))
+                field[i][r] = flip(field[i][r])
+                h = find_reflections(field, original_h)
+                if h:
+                    return h
+                field[i][r] = flip(field[i][r])
+        return h
+
+
+sum = 0
+for field in fields:
+    sum += 100 * smudge(field)
+    field = [list(x) for x in zip(*field)]
+    sum += smudge(field)
+
+print(sum)
