a50832cde432ffb2a3b8b6190b18dc2e87c7a96c
Gyula Matics <mgyula@gmail.com>
2023-12-18 22:43:48 +0100

    Day 18

--- day18/part2.py
+++ day18/part2.py
@@ -1,10 +1,10 @@
-instructions = [x.split(" ") for x in open('input.txt').read().splitlines()]
+instructions = [(x.split(" ")[2][7], int(x.split(" ")[2][2:7], 16)) for x in open('input.txt').read().splitlines()]
 
 movements = {
-    "R": (0, 1,),
-    "D": (1, 0),
-    "L": (0, -1),
-    "U": (-1, 0)
+    "0": (0, 1,),
+    "1": (1, 0),
+    "2": (0, -1),
+    "3": (-1, 0)
 }
 
 
@@ -12,24 +12,23 @@ def dig(maxy, test):
     x = 0
     y = 0
     area = 0
-    for i in range(len(instructions)-1):
+    for i in range(len(instructions) - 1):
         inst = instructions[i]
-        nextinst = instructions[i+1]
-        previnst = instructions[i-1]
+        nextinst = instructions[i + 1]
+        previnst = instructions[i - 1]
 
-        dir, steps, color = inst
-        nextdir, *_ = nextinst
-        prevdir, *_ = previnst
+        dir, steps = inst
+        nextdir, _ = nextinst
+        prevdir, _ = previnst
 
         dy, dx = movements[dir]
         ndy, ndx = movements[nextdir]
         pdy, pdx = movements[prevdir]
 
-        steps = int(steps)
         y += dy * steps
         x += dx * steps
 
-        if not test and dx!=0: # trace outer edge
+        if not test and dx != 0:  # trace outer edge
             if dx == 1:
                 outeryedgelength = maxy - y + 1
             if dx == -1:
@@ -38,10 +37,10 @@ def dig(maxy, test):
             outerxedgelength = steps + 1
             if ndy == -dx:
                 outerxedgelength -= 1
-            if pdy == dx :
+            if pdy == dx:
                 outerxedgelength -= 1
 
-            area += outeryedgelength*outerxedgelength*dx
+            area += outeryedgelength * outerxedgelength * dx
 
         if test:
             maxy = max(y, maxy)
